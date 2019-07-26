b623a14c445ca6d6b965b4bf44ae4b16e010a5e9
Javier Torres <javier.torresb@gmail.com>
2019-07-26 16:41:01 -0400

    New GkPos Python Lib

--- integration_test/gkControl.py
+++ integration_test/gkControl.py
@@ -11,6 +11,12 @@ from LibTaca.TacaTools import Homing
 from LibTaca.TacaTools import WriteExcel
 from LibTaca.utilsmod import start_liveplotter2 as lp2
 
+import LibTaca.GkPos as gkp
+import cv2
+
+# Create a GkPos object with an array size of 5
+gkPos = gkp.GkPos(5)
+
 print("finding an odrive...")
 my_drive = odrive.find_any()
 A0 = my_drive.axis0
@@ -32,8 +38,28 @@ my_drive.axis0.motor.config.current_lim = 25 # Amperes de corriente limite
 print ("Homing...\n")
 Rango, Centro, MaxPos, MinPos = Homing(my_drive)
 
-cp = my_drive.axis0.encoder.pos_estimate
+time.sleep(10)
+while(True):
+	# Get the actual pos
+	p = (0,0) # replace with call to ball detector
 
-print ("Rango: ", Rango, "Centro: ", Centro, "MaxPos: ", MaxPos, "MinPos ", MinPos, "\n")
+	# Pass it to GkPos 
+	gkPos.push(p)
 
-56, 4.5, 22.5, 34.5, 35.5
+	# Get an updated estimate for the best goalkeeper position
+	(x,y) = gkPos.get_estimate()
+
+	# Convert it to Motor Control coordinates
+	y_ref = (MaxPos-abs(0.05*MaxPos)-(MinPos+abs(0.05*MinPos)))/gkPos.get_const()*y + (MinPos+abs(0.05*MinPos))
+
+	# Constrain the reference once again
+	if y_ref > MaxPos:
+		y_ref = MaxPos
+	elif y_ref < MinPos:
+		y_ref = MinPos
+
+	# Send the motor to the required position
+	A0.controller.pos_setpoint = y_ref
+	k = cv2.waitKey(1)
+	if k == 27:
+		break
